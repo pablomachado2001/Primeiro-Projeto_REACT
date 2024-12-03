191b8641f18c87af193382790ca6558b3f6f28f8
Pablo Machado <machadopablo27@gmail.com>
2024-12-03 16:39:32 -0300

    Aplicando CSS

--- src/components/Perfil.js
+++ src/components/Perfil.js
@@ -1,10 +1,12 @@
+import styles from './Perfil.module.css'
+
 function Perfil({imagem, nome, idade, cor}) {
  return (
-    <div>
-        <img src={imagem} alt={nome}/>
-        <h2>Bem vindo, {nome}!</h2>
-        <p>Idade: {idade}</p>
-        <p>Sua cor favorita é: {cor}</p>
+    <div className={styles.container}>
+        <img className={styles.imagemPerfil} src={imagem} alt={nome}/>
+        <h2 className={styles.nomeComMensagem}>Bem vindo, {nome}!</h2>
+        <p className={styles.idade}>Idade: {idade}</p>
+        <p className={styles.cor}>Sua cor favorita é: {cor}</p>
     </div>
  )
 }
