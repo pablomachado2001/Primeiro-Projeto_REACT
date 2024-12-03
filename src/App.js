6932bbaa8e7f0949efe53aa4fd5d92838ca57509
Pablo Machado <machadopablo27@gmail.com>
2024-12-03 16:18:34 -0300

    Alterando a cor do Michelangelo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const imagem = "https://tm.ibxk.com.br/2014/08/13/13153232058627.jpg?ims=1200x675";
   const nome = "Michelangelo";
   const idade = 15;
-  const cor = "Laranja"
+  const cor = "Laranja";
   return (
     <div className="App">
       <Perfil imagem={imagem} nome={nome} idade={idade} cor={cor}/>
